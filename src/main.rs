75ed17c59deb87bae6f095b97c2fa7042fbd61e4
f3 <f3kilo@yandex.ru>
2022-04-28 18:43:37 +0300

    State

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let mut state = State::Menu;
+
+    loop {
+        let new_state = state.update();
+        if let State::Exit = new_state {
+            break;
+        }
+
+        state = new_state;
+    }
+}
+
+enum State {
+    Menu,
+    Guessing(u32),
+    Exit,
+}
+
+impl State {
+    pub fn update(&self) -> Self {
+        todo!()
+    }
 }
